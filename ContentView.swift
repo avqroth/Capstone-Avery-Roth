5f0f3ebf319f10b5336ea3cb61153f8163f3b0cc
avqroth <142459501+avqroth@users.noreply.github.com>
2024-06-10 13:36:22 -0500

    1st commit

--- ContentView.swift
+++ ContentView.swift
@@ -19,38 +19,16 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+            NavigationStack {
+                Text("The Solar System")
+                    .font(Font.custom("Avenir", size: 100))
+                    .padding(.top)
+            }
 
-            Text("Hello, world!")
 
-            Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
-                .font(.title)
-                .frame(width: 360)
-                .padding(24)
-                .glassBackgroundEffect()
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
-                }
             }
         }
     }
-}
 
 #Preview(windowStyle: .automatic) {
     ContentView()
---
+++ Models/Planets.swift
@@ -0,0 +1,29 @@
+//
+//  Planets.swift
+//  Capstone-Avery
+//
+//  Created by Avery Roth on 6/10/24.
+//
+
+import Foundation
+
+class Planets: ObservableObject {
+    let Sun = "The Sun is the largest object in our solar system. Its diameter is about 865,000 miles (1.4 million kilometers). Its gravity holds the solar system together, keeping everything from the biggest planets to the smallest bits of debris in orbit around it."
+
+    let Mercury = "The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth's Moon. From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter."
+
+    let Venus = "Venus is the second planet from the Sun, and Earth's closest planetary neighbor. It's the hottest planet in our solar system, and it is sometimes called Earth's twin."
+
+    let Earth = "Earth – our home planet – is the third planet from the Sun, and the fifth largest planet. It's the only place we know of inhabited by living things."
+
+    let Mars = "Mars – the fourth planet from the Sun – is a dusty, cold, desert world with a very thin atmosphere. This dynamic planet has seasons, polar ice caps, extinct volcanoes, canyons and weather."
+
+    let Jupiter = "Jupiter is the largest planet in our solar system – if it were a hollow shell, 1,000 Earths could fit inside. It's also the oldest planet, forming from the dust and gases left over from the Sun's formation 4.5 billion years ago."
+
+    let Saturn = "Saturn is the sixth planet from the Sun, and the second-largest planet in our solar system. Saturn is surrounded by a spectacular ring system, and it has dozens of moons."
+
+    let Uranus = "Uranus is the seventh planet from the Sun, and it has the third largest diameter of planets in our solar system. Uranus appears to spin sideways."
+
+    let Neptune = "Neptune is the eighth and most distant planet in our solar system. Dark, cold, and whipped by supersonic winds, ice giant Neptune is more than 30 times as far from the Sun as Earth."
+
+}
